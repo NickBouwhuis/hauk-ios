19022d9863e1d7bd761fa8c5131e4df360353418
Nick Bouwhuis <nick@bouwhuis.net>
2024-11-08 20:36:44 +0100

    Initial Commit

---
+++ Hauk/HaukApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaukApp.swift
+//  Hauk
+//
+//  Created by Nick Bouwhuis on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HaukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HaukTests/HaukTests.swift
@@ -0,0 +1,17 @@
+//
+//  HaukTests.swift
+//  HaukTests
+//
+//  Created by Nick Bouwhuis on 08/11/2024.
+//
+
+import Testing
+@testable import Hauk
+
+struct HaukTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
